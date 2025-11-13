e6ba1995e327e5491c0a12354e2c1e7523d02028
Joowyy <joelsanchezfernandez05@gmail.com>
2025-11-13 19:15:38 +0100

    Primer Commit

---
+++ app/src/main/java/com/example/joelsanchez_centrodeportivo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.joelsanchez_centrodeportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
